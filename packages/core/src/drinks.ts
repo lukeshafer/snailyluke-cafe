f6f11a999f125524004375d57328b18a459561ff
Luke <lukealanshafer@gmail.com>
2024-03-23 22:10:41 -0400

    Migrate to SST Ion

--- packages/core/src/drinks.ts
+++ packages/core/src/drinks.ts
@@ -1,61 +1,61 @@
-import { randomUUID } from 'crypto';
-import { Entity, EntityItem, type EntityConfiguration } from 'electrodb';
-import { Table } from 'sst/node/table';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { randomUUID } from "crypto";
+import { Entity, EntityItem, type EntityConfiguration } from "electrodb";
+import { Resource } from "sst";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export type Drink = EntityItem<typeof drinks>;
 
 export const config = {
-  table: Table.database.tableName,
-  client: new DynamoDBClient(),
+	table: Resource.CafeDatabase.name,
+	client: new DynamoDBClient(),
 } satisfies EntityConfiguration;
 
 export const drinks = new Entity(
-  {
-    model: {
-      entity: 'drink',
-      service: 'snailyluke-cafe',
-      version: '1',
-    },
-    attributes: {
-      username: {
-        type: 'string',
-        required: true,
-      },
-      displayName: {
-        type: 'string',
-        required: true,
-      },
-      drinkName: {
-        type: 'string',
-        required: true,
-      },
-      fileName: {
-        type: 'string',
-        required: true,
-      },
-      isServed: {
-        type: 'boolean'
-      },
-      orderedAt: {
-        type: 'number',
-        default: () => Date.now(),
-        // cannot be modified after created
-        readOnly: true,
-      },
-    },
-    indexes: {
-      primary: {
-        pk: {
-          field: 'pk',
-          composite: ['username'],
-        },
-        sk: {
-          field: 'sk',
-          composite: [],
-        },
-      },
-    },
-  },
-  config
+	{
+		model: {
+			entity: "drink",
+			service: "snailyluke-cafe",
+			version: "1",
+		},
+		attributes: {
+			username: {
+				type: "string",
+				required: true,
+			},
+			displayName: {
+				type: "string",
+				required: true,
+			},
+			drinkName: {
+				type: "string",
+				required: true,
+			},
+			fileName: {
+				type: "string",
+				required: true,
+			},
+			isServed: {
+				type: "boolean",
+			},
+			orderedAt: {
+				type: "number",
+				default: () => Date.now(),
+				// cannot be modified after created
+				readOnly: true,
+			},
+		},
+		indexes: {
+			primary: {
+				pk: {
+					field: "pk",
+					composite: ["username"],
+				},
+				sk: {
+					field: "sk",
+					composite: [],
+				},
+			},
+		},
+	},
+	config,
 );
--- sst.config.ts
+++ sst.config.ts
@@ -1,17 +1,79 @@
-import { SSTConfig } from 'sst';
-import { API } from './stacks/API';
-import { Database } from './stacks/Database';
-import { Events } from './stacks/Events';
-import { Overlay } from './stacks/Overlay';
+/// <reference path="./.sst/platform/config.d.ts" />
 
-export default {
-	config(_input) {
+export default $config({
+	app() {
 		return {
-			name: 'snailyluke-cafe',
-			region: 'us-east-2',
+			name: "snailyluke-cafe",
+			home: "aws",
+			providers: {
+				aws: {
+					region: "us-east-2",
+				},
+			},
 		};
 	},
-	stacks(app) {
-		app.stack(Database).stack(Events).stack(API).stack(Overlay);
+	async run() {
+		//new sst.aws.Bucket("MyBucket");
+		//
+		//old stack:
+		//  Database > Event > API > Overlay
+
+		const drinkDatabase = new sst.aws.Dynamo("CafeDatabase", {
+			fields: {
+				pk: "string",
+				sk: "string",
+				gsi1pk: "string",
+				gsi1sk: "string",
+			},
+			primaryIndex: {
+				hashKey: "pk",
+				rangeKey: "sk",
+			},
+			globalIndexes: {
+				gsi1: {
+					hashKey: "gsi1pk",
+					rangeKey: "gsi1sk",
+				},
+			},
+		});
+
+		// TODO: add event bus based on the below code
+		// const bus = new sst.aws.EventBus("EventBus", {
+		//  defaults: {
+		//    retries: 10,
+		//  },
+		// });
+		//  bus.subscribe('drink.ordered', {
+		//    handler: 'packages/functions/src/events/drink-ordered.handler',
+		//    bind: [table],
+		//  });
+
+		const api = new sst.aws.ApiGatewayV2("CafeAPI")
+			.route("GET /drink/{drinkname}", {
+				handler: "packages/functions/src/api/order-drink.handler",
+				link: [drinkDatabase],
+			})
+			.route("GET /drinks", {
+				handler: "packages/functions/src/api/drinks.handler",
+				link: [drinkDatabase],
+			})
+			.route("GET /menu", {
+				handler: "packages/functions/src/api/menu.handler",
+			})
+			.route("PATCH /drink/{drinkname}", {
+				handler: "packages/functions/src/api/update-drink.handler",
+				link: [drinkDatabase],
+			});
+
+		const overlay = new sst.aws.StaticSite("CafeOverlay", {
+			path: "packages/overlay",
+			build: {
+				command: "pnpm run build",
+				output: "dist",
+			},
+			environment: {
+				VITE_APP_API_URL: api.url,
+			},
+		});
 	},
-} satisfies SSTConfig;
+});
